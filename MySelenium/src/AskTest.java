931523f501abbfc54c94747d65f5e2bc9ce2bf5f
Tom Mac <tomvmac@yahoo.com>
2013-12-27 08:27:44 -0500

    Added AskTest.java

---
+++ MySelenium/src/AskTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class AskTest {
+    @Test
+    public void shouldAskGoogleForWebDriver() {
+        // Setup chrome driver
+        System.setProperty("webdriver.chrome.driver", "C:\\DevTools\\chromedriver_win32\\chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+
+
+        browser.get("http://www.bing.com/");
+
+        WebElement inputField = browser.findElement(By.id("sb_form_q"));
+        inputField.sendKeys("Samsung Galaxy Tab 3");
+
+        WebElement searchButton = browser.findElement(By.id("sb_form_go"));
+        searchButton.click();
+
+        browser.close();
+    }
+}
