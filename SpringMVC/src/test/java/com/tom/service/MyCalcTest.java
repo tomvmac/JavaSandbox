f8bd29029e54231f15b54963fe6c0adbd9815df9
Tom Mac <tomvmac@yahoo.com>
2014-03-30 15:17:20 -0400

    Added nexus repo to spring mvc project and reference to jar project.

--- SpringMVC/src/test/java/com/tom/service/MyCalcTest.java
+++ SpringMVC/src/test/java/com/tom/service/MyCalcTest.java
@@ -2,6 +2,7 @@ package com.tom.service;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import com.tommac.MyCalc;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,9 +18,10 @@ public class MyCalcTest {
     public void TestAddNumbers() {
         int a = 1;
         int b = 3;
-        int expectedResult = 4;
+        int c = 3;
+        int expectedResult = 7;
 
-        int actualResult = MyCalc.addNumbers(a, b);
+        int actualResult = MyCalc.addNumbers(a, b, c);
 
         assertEquals(actualResult, expectedResult);
 
